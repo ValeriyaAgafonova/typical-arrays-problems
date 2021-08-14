1018685f95469760abb24c6548df8c655cbd1860
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2021-08-14 16:25:24 +0300

    init: resolve tasinit: resolve taskk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+  if (!array || 0 === array.length){
   return 0;
+  }
+  else {
+  let x = Math.min.apply(0,array)
+  return x;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || 0 === array.length){
+    return 0;
+    }
+    else {
+      let y = Math.max.apply(0, array);
+      return y;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || 0 === array.length){
+    return 0;
+    }
+    else {
+      let sum = 0;
+      for (let i = 0; i < array.length; i++){
+        sum += array[i];
+      }
+      let x = sum / array.length;
+      return x;
+    }
 }
